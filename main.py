206850654a132424ec5b262f3094d2206a04b162
tarako676767 <degunaoko77@gmail.com>
2026-09-21 19:33:05 +0900

    Update and rename main_bot.py to main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import uuid
 import logging
 import functools
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
